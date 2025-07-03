54650f6cdc31a98508aad5c915937d32aa79cad8
Justin Jenkins <justinjenkins@gmail.com>
2025-07-03 15:51:48 -0600

    Fix ImagePreview to maintain 16:9 aspect ratio for image area with max height constraint

--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -10,15 +10,17 @@ interface ImagePreviewProps {
 const ImagePreview = ({ id, title, description }: ImagePreviewProps) => (
   <ChakraLink as={NextLink} href={`/view/${id}`} _hover={{ textDecoration: 'none' }}>
     <Box boxShadow="md" borderRadius="lg" overflow="hidden" bg="blackAlpha.700">
-      <Image
-        src={`/resources/${id}.jpg`}
-        alt={title}
-        width="100%"
-        height="200px"
-        objectFit="cover"
-        display="block"
-        borderTopRadius="lg"
-      />
+      <Box position="relative" width="100%" aspectRatio="16/9" maxHeight="300px">
+        <Image
+          src={`/resources/${id}.jpg`}
+          alt={title}
+          width="100%"
+          height="100%"
+          objectFit="cover"
+          display="block"
+          borderTopRadius="lg"
+        />
+      </Box>
       <Box p={3}>
         <Text fontWeight="bold" fontSize="md" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">{title}</Text>
         {description && <Text fontSize="sm" color="whiteAlpha.700" overflow="hidden" textOverflow="ellipsis" whiteSpace="normal" maxH="2.5em" lineHeight="1.25em">{description}</Text>}
