9049ee43be4a45b43ca2f11b36f1d1dbd8726b0e
Justin Jenkins <justinjenkins@gmail.com>
2025-07-03 13:02:11 -0600

    UI: Add opacity and fixed height to ModelResponsesModal tab content, adjust color scheme for tabs

--- components/ModelResponsesModal.tsx
+++ components/ModelResponsesModal.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import GlassyModal from './GlassyModal';
-import JsonViewer from './JsonViewer';
 import { RawModelResponse } from '@/types/image';
-import { Box } from '@chakra-ui/react';
+import { Box, Text, Tabs } from '@chakra-ui/react';
 
 interface ModelResponsesModalProps {
   isOpen: boolean;
@@ -17,17 +16,34 @@ export default function ModelResponsesModal({ isOpen, onClose, responses }: Mode
       <Box w="100%" display="flex" justifyContent="center">
         <Box
           bg="gray.900"
+          opacity={0.7}
           color="gray.100"
           borderRadius="md"
           p={4}
-          fontFamily="mono"
-          fontSize="sm"
+          fontFamily="body"
+          fontSize="md"
           boxShadow="md"
           w="100%"
+          h="500px"
           maxW="600px"
           overflowX="auto"
         >
-          <JsonViewer data={{ responses }} />
+          <Tabs.Root defaultValue={responses[0]?.model || ''} variant="subtle" colorScheme="dark" fitted>
+            <Tabs.List>
+              {responses.map((r) => (
+                <Tabs.Trigger key={r.model} value={r.model}>
+                  {r.model}
+                </Tabs.Trigger>
+              ))}
+            </Tabs.List>
+            {responses.map((r) => (
+              <Tabs.Content key={r.model} value={r.model} style={{ minHeight: '180px', display: 'flex', alignItems: 'flex-start' }}>
+                <Text whiteSpace="pre-wrap" p={4} w="100%">
+                  {r.response}
+                </Text>
+              </Tabs.Content>
+            ))}
+          </Tabs.Root>
         </Box>
       </Box>
     </GlassyModal>
